7a619f36b701d618d6e37a3f9db121ebbe080b36
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-01-14 09:40:06 -0600

    added `TileScanning` helper type + v1.0.2 update

---
+++ src/API/Edits/NearbyEffectsBlocking.cs
@@ -0,0 +1,30 @@
+﻿using MonoMod.RuntimeDetour.HookGen;
+using SerousCommonLib.API.Helpers;
+using System.Reflection;
+using Terraria.ModLoader;
+
+namespace SerousCommonLib.API.Edits {
+	internal class NearbyEffectsBlocking : Edit {
+		private static readonly MethodInfo TileLoader_NearbyEffects = typeof(TileLoader).GetMethod(nameof(TileLoader.NearbyEffects), BindingFlags.Public | BindingFlags.Static);
+
+		private delegate void orig_TileLoader_NearbyEffects(int i, int j, int type, bool closer);
+		private delegate void hook_TileLoader_NearbyEffects(orig_TileLoader_NearbyEffects orig, int i, int j, int type, bool closer);
+		private static event hook_TileLoader_NearbyEffects On_TileLoader_NearbyEffects {
+			add => HookEndpointManager.Add<hook_TileLoader_NearbyEffects>(TileLoader_NearbyEffects, value);
+			remove => HookEndpointManager.Remove<hook_TileLoader_NearbyEffects>(TileLoader_NearbyEffects, value);
+		}
+
+		public override void LoadEdits() {
+			On_TileLoader_NearbyEffects += Hook_TileLoader_NearbyEffects;
+		}
+
+		public override void UnloadEdits() {
+			On_TileLoader_NearbyEffects -= Hook_TileLoader_NearbyEffects;
+		}
+
+		private void Hook_TileLoader_NearbyEffects(orig_TileLoader_NearbyEffects orig, int i, int j, int type, bool closer) {
+			if (!TileScanning.DoBlockHooks)
+				orig(i, j, type, closer);
+		}
+	}
+}
---
+++ src/API/Helpers/TileScanning.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace SerousCommonLib.API.Helpers {
+	/// <summary>
+	/// A helper class for retrieving biome information around a given area
+	/// </summary>
+	public static class TileScanning {
+		internal static bool DoBlockHooks;
+
+		/// <summary>
+		/// Performs biome scanning with <paramref name="worldCenter"/> as the center of the scanning area
+		/// </summary>
+		/// <param name="worldCenter">The world coordinates for the center of the scanning area</param>
+		/// <returns>A <see cref="SceneMetrics"/> instance containing biome information, or <see langword="null"/> if an exception was caught</returns>
+		public static SceneMetrics Scan(Vector2 worldCenter) {
+			try {
+				SceneMetrics sceneMetrics = new();
+
+				SceneMetricsScanSettings settings = new SceneMetricsScanSettings {
+					VisualScanArea = null,
+					BiomeScanCenterPositionInWorld = worldCenter,
+					ScanOreFinderData = false
+				};
+
+				DoBlockHooks = true;
+
+				sceneMetrics.ScanAndExportToMain(settings);
+
+				DoBlockHooks = false;
+
+				// Reset the player's biomes to reset
+				Main.LocalPlayer.ForceUpdateBiomes();
+
+				return sceneMetrics;
+			} catch {
+				// Swallow any exceptions
+				return null;
+			}
+		}
+	}
+}
