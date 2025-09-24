7c1df8a537498db7c1b0829cb485d89a4f078808
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2025-09-23 23:01:46 -0500

    add new extension for formatting type names

--- src/API/Extensions/Extensions.Type.cs
+++ src/API/Extensions/Extensions.Type.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace SerousCommonLib.API {
 	partial class Extensions {
@@ -21,6 +23,79 @@ namespace SerousCommonLib.API {
 
 			//Construct the child types
 			return $"{parent}<{string.Join(", ", type.GetGenericArguments().Select(GetSimplifiedGenericTypeName))}>";
+		}
+
+		private static readonly Dictionary<Type, string> dealias = new() {
+			[typeof(bool)] = "bool",
+			[typeof(sbyte)] = "sbyte",
+			[typeof(short)] = "short",
+			[typeof(int)] = "int",
+			[typeof(long)] = "long",
+			[typeof(byte)] = "byte",
+			[typeof(ushort)] = "ushort",
+			[typeof(uint)] = "uint",
+			[typeof(ulong)] = "ulong",
+			[typeof(float)] = "float",
+			[typeof(double)] = "double",
+			[typeof(decimal)] = "decimal",
+			[typeof(char)] = "char",
+			[typeof(string)] = "string",
+			[typeof(IntPtr)] = "IntPtr",
+			[typeof(UIntPtr)] = "UIntPtr",
+			[typeof(object)] = "object"
+		};
+
+		/// <summary>
+		/// Gets a C#-like full name of <paramref name="type"/>.
+		/// </summary>
+		/// <param name="type">The type</param>
+		/// <returns>A C#-like full name of <paramref name="type"/></returns>
+		/// <exception cref="ArgumentException"/>
+		public static string FullNameUpgraded(this Type type) {
+			if (type.IsGenericTypeDefinition)
+				throw new ArgumentException("Type argument was a type definition");
+
+			if (!type.IsGenericType)
+				return dealias.TryGetValue(type, out string name) ? name : type.Name;
+
+			StringBuilder sb;
+			const int size = 20;
+
+			void GetSubTypesString() {
+				var subTypes = type.GetGenericArguments();
+
+				sb.Append(subTypes[0].FullNameUpgraded());
+				for (int i = 1; i < subTypes.Length; i++) {
+					sb.Append(", ");
+					sb.Append(subTypes[i].FullNameUpgraded());
+				}
+			}
+
+			//Special treatment for Nullable<T> and ValueTuple<>
+			if (type.IsGenericType) {
+				var genDef = type.GetGenericTypeDefinition();
+
+				if (genDef == typeof(Nullable<>))
+					return type.GetGenericArguments()[0].FullNameUpgraded() + "?";
+				else if (genDef == typeof(ValueTuple<>)) {
+					sb = new StringBuilder(size);
+
+					sb.Append('(');
+					GetSubTypesString();
+					sb.Append(')');
+
+					return sb.ToString();
+				}
+			}
+
+			sb = new StringBuilder(size);
+
+			sb.Append(type.Name.AsSpan(0, type.Name.IndexOf('`')));
+			sb.Append('<');
+			GetSubTypesString();
+			sb.Append('>');
+
+			return sb.ToString();
 		}
 	}
 }
