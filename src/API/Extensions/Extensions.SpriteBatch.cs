af7d1a6dccb2a2d2578f06a6030ef39c8148f487
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2025-09-25 15:19:02 -0500

    added SpriteBatchCapture and SpriteBatch extension methods

---
+++ src/API/Extensions/Extensions.SpriteBatch.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace SerousCommonLib.API;
+
+partial class Extensions {
+	public static void Begin(this SpriteBatch @this, SpriteBatchCapture capture) {
+		@this.Begin(
+			capture.sortMode,
+			capture.blendState,
+			capture.samplerState,
+			capture.depthStencilState,
+			capture.rasterizerState,
+			capture.customEffect,
+			capture.transformMatrix
+		);
+	}
+	
+	public static bool IsActive(this SpriteBatch @this) => @this.beginCalled;
+}
---
+++ src/API/SpriteBatchCapture.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SerousCommonLib.API;
+
+/// <summary>
+/// An object representing a capture of the parameters for a <see cref="SpriteBatch"/> instance.
+/// </summary>
+public class SpriteBatchCapture {
+	// 1-to-1 mapping to the fields affected by SpriteBatch.Begin()
+
+	public readonly SpriteSortMode sortMode;
+	public readonly BlendState blendState;
+	public readonly SamplerState samplerState;
+	public readonly DepthStencilState depthStencilState;
+	public readonly RasterizerState rasterizerState;
+	public readonly Effect customEffect;
+	public readonly Matrix transformMatrix;
+
+	public SpriteBatchCapture(SpriteBatch spriteBatch) {
+		sortMode = spriteBatch.sortMode;
+		blendState = spriteBatch.blendState;
+		samplerState = spriteBatch.samplerState;
+		depthStencilState = spriteBatch.depthStencilState;
+		rasterizerState = spriteBatch.rasterizerState;
+		customEffect = spriteBatch.customEffect;
+		transformMatrix = spriteBatch.transformMatrix;
+	}
+}
